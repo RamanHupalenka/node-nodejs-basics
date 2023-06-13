d5e6e7dca6c46b6e7ed8a494f8b45231927b8a66
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-13 20:39:30 +0300

    feat: implement fs read functionality

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,36 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { access, readFile } from 'fs/promises';
+import { constants } from 'fs';
+
+const getCurrentFileInfo = (fileUrl) => {
+    const __filename = fileURLToPath(fileUrl);
+    const __dirname = dirname(__filename);
+
+    return [__dirname, __filename];
+};
+
+const checkForFileExistence = async (pathToFile) => {
+    try {
+        await access(pathToFile, constants.R_OK);
+    } catch {
+        throw new Error('FS operation failed');
+    }
+};
+
 const read = async () => {
-    // Write your code here 
+    const [dirname] = getCurrentFileInfo(import.meta.url);
+    const pathToFile = resolve(dirname, './files/fileToRead.txt');
+
+    await checkForFileExistence(pathToFile);
+
+    try {
+        const fileData = await readFile(pathToFile);
+
+        console.log(fileData.toString());
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
