ac3436bf3602527fa0e7e7e07a27ef806914afa6
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-14 17:22:29 +0300

    refactor: update fs read logic related to file existance check

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,12 +1,16 @@
 import { resolve } from 'path';
 import { readFile } from 'fs/promises';
-import { checkForFileExistence, getCurrentFileInfo } from '../utils/fs.js';
+import { isFileExists, getCurrentFileInfo } from '../utils/fs.js';
 
 const read = async () => {
     const [dirname] = getCurrentFileInfo(import.meta.url);
     const pathToFile = resolve(dirname, './files/fileToRead.txt');
 
-    await checkForFileExistence(pathToFile);
+    const isTargetFileDoesNotExists = !(await isFileExists(pathToFile));
+
+    if (isTargetFileDoesNotExists) {
+        throw new Error('FS operation failed');
+    }
 
     try {
         const fileData = await readFile(pathToFile);
