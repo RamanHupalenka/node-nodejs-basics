83cb6820b67f974855c3b407c40ec411f59ae336
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-14 17:41:16 +0300

    feat: implement fs delete functionality

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { resolve } from 'path';
+import { rm as removeFile } from 'fs/promises';
+import { isFileExists, getCurrentFileInfo } from '../utils/fs.js';
+
 const remove = async () => {
-    // Write your code here 
+    const [dirname] = getCurrentFileInfo(import.meta.url);
+    const pathToFile = resolve(dirname, './files/fileToRemove.txt');
+
+    const isTargetFileDoesNotExists = !(await isFileExists(pathToFile));
+
+    if (isTargetFileDoesNotExists) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await removeFile(pathToFile);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
