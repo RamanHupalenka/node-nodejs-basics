ae62481088decd96dd3acf5a243257a522a85e08
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-14 18:00:56 +0300

    feat: implement fs copy functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { resolve } from 'path';
+import { cp as copyFolder } from 'fs/promises';
+import { isFileExists, getCurrentFileInfo } from '../utils/fs.js';
+
 const copy = async () => {
-    // Write your code here 
+    const [dirname] = getCurrentFileInfo(import.meta.url);
+    const pathToSourceFolder = resolve(dirname, './files');
+    const pathToTargetFolder = resolve(dirname, './files_copy');
+
+    const isSourceFolderDoesNotExists = !(await isFileExists(pathToSourceFolder));
+    const isTargetFolderExists = await isFileExists(pathToTargetFolder);
+
+    if (isSourceFolderDoesNotExists || isTargetFolderExists) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await copyFolder(pathToSourceFolder, pathToTargetFolder, {
+            recursive: true,
+        });
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
