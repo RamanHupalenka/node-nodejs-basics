0d878d23a9d04200968bf94e6e0fa5f43b58b18f
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-14 17:47:20 +0300

    feat: implement fs create functionality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { resolve } from 'path';
+import { writeFile } from 'fs/promises';
+import { isFileExists, getCurrentFileInfo } from '../utils/fs.js';
+
 const create = async () => {
-    // Write your code here 
+    const [dirname] = getCurrentFileInfo(import.meta.url);
+    const pathToFile = resolve(dirname, './files/fresh.txt');
+
+    const isTargetFileExists = await isFileExists(pathToFile);
+
+    if (isTargetFileExists) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await writeFile(pathToFile, 'I am fresh and young');
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
