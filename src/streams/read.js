dbd0a90a0c6aa5226943c37e8b290b52cf7f0f65
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-17 10:57:49 +0300

    feat: implement streams read functionality

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,28 @@
+import { resolve } from 'path';
+import { open } from 'fs/promises';
+import { isFileOrFolderExists, getCurrentFileInfo } from '../utils/fs.js';
+
 const read = async () => {
-    // Write your code here 
+    const [__dirname] = getCurrentFileInfo(import.meta.url);
+    const pathToFile = resolve(__dirname, './files/fileToRead.txt');
+
+    const isTargetFileDoesNotExists = !(await isFileOrFolderExists(pathToFile));
+
+    if (isTargetFileDoesNotExists) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        const fileData = await open(pathToFile, 'r');
+
+        const readStream = fileData.createReadStream({
+            autoClose: true,
+        });
+
+        readStream.pipe(process.stdout);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
