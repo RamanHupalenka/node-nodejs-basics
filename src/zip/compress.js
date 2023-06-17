c7cc2e40656880abf0cc2c6ee3fa6ea3370b85fe
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-17 12:31:28 +0300

    feat: implement zip compress functionality

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,42 @@
+import { resolve } from 'path';
+import { open, rm as removeFile } from 'fs/promises';
+import { createGzip } from 'zlib';
+import { isFileOrFolderExists, getCurrentFileInfo } from '../utils/fs.js';
+
 const compress = async () => {
-    // Write your code here 
+    const [__dirname] = getCurrentFileInfo(import.meta.url);
+    const pathToSourceFile = resolve(__dirname, './files/fileToCompress.txt');
+    const pathToTargetArchive = resolve(__dirname, './files/archive.gz');
+
+    const isSourceFileDoesNotExists = !(await isFileOrFolderExists(pathToSourceFile));
+    const isTargetArchiveExists = await isFileOrFolderExists(pathToTargetArchive);
+
+    if (isSourceFileDoesNotExists || isTargetArchiveExists) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        const fileData = await open(pathToSourceFile, 'r');
+        const archiveData = await open(pathToTargetArchive, 'w');
+
+        const gzip = createGzip();
+
+        const readStream = fileData.createReadStream({
+            autoClose: true,
+        });
+
+        const writeStream = archiveData.createWriteStream({
+            autoClose: true,
+        });
+
+        readStream.pipe(gzip).pipe(writeStream);
+
+        readStream.on('end', () => {
+            removeFile(pathToSourceFile);
+        });
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await compress();
