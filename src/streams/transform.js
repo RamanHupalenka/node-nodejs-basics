5bb67070d71a1fcd7d6c8ab72a1b5193b66fd172
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-17 11:43:07 +0300

    feat: implement streams transform functionality

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
+import { Transform } from 'stream';
+
+const reverseStringExcludeEOL = (str) => {
+    const endOfLineCRLFRegExp = /\\r\\n/;
+    const endOfLine = endOfLineCRLFRegExp.test(str) ? '\r\n' : '\n';
+    const strWithoutEOL = str.replace(endOfLine, '');
+
+    return `${strWithoutEOL.split('').reverse().join('')}${endOfLine}`;
+};
+
+const transformStream = new Transform({
+    transform(chunk, _, callback) {
+        callback(null, reverseStringExcludeEOL(String(chunk)));
+    },
+});
+
 const transform = async () => {
-    // Write your code here 
+    try {
+        process.stdin.pipe(transformStream).pipe(process.stdout);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await transform();
