73f45bc06c9530e5645fa589df46dd88593d831b
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-18 11:36:05 +0300

    feat: implement cp functionality

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,27 @@
+import { resolve } from 'path';
+import { spawn } from 'child_process';
+import { isFileOrFolderExists, getCurrentFileInfo } from '../utils/fs.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const [__dirname] = getCurrentFileInfo(import.meta.url);
+    const pathToScript = resolve(__dirname, './files/script.js');
+
+    const isTargetScriptDoesNotExists = !(await isFileOrFolderExists(pathToScript));
+
+    if (isTargetScriptDoesNotExists) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        const cp = spawn('node', [pathToScript, ...args]);
+
+        process.stdin.pipe(cp.stdin);
+
+        cp.stdout.pipe(process.stdout);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
